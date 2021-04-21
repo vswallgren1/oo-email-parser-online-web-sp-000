b36346252e9d164c2184cabbd8af67ec98a40d2b
Vince W <vincewallgren@me.com>
2021-04-21 04:48:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,4 +6,16 @@
 
 class EmailAddressParser
 
-end
+  attr_accessor :name, :csv_emails
+
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
+
+  def parse
+    csv_emails.split.collect do |address|
+      address.split(',')
+    end
+    .flatten.uniq
+  end
+end 
